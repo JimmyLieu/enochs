adc8f806f616b03c6f2a7c873b49a46b53a95a08
Jimmy Lieu <98052856+JimmyLieu@users.noreply.github.com>
2024-11-04 23:44:07 -0600

    Video_Import

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,17 +2,21 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
+import coffeeVideo from '../assets/Coffee.mp4';
 
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <video src='./' autoPlay loop muted />
-            console.log(test)
+            <video src={coffeeVideo} autoPlay loop muted />
             <h1> TEST MESSAGE </h1>
             <p> TESTERRRRR</p>
-            <div className="hero-btns">
-                <Button className='btns' buttonStyle='btn--outline'
-                buttonSize='btn--large'>MENU</Button>
+            <div className='hero-btns'>
+                <Button 
+                className='btns' 
+                buttonStyle='btn--outline'
+                buttonSize='btn--large'>
+                    MENU
+                    </Button>
                 <Button className='btns' buttonStyle='btn--outline'
                 buttonSize='btn--large'>MENU 2</Button>
             </div>
